794bf8a0d13e598fb08ca99a82e1ac0274bec9d9
Sudarsana K <sudak80@gmail.com>
2023-03-12 14:37:25 +0530

    commit4

--- ec2-ebs/5-ec2-module.tf
+++ ec2-ebs/5-ec2-module.tf
@@ -20,7 +20,8 @@ module "security_group" {
   vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  ingress_rules       = ["all-all"]
+  #ingress_rules       = ["http-80-tcp", "all-icmp"]
   egress_rules        = ["all-all"]
 
   tags = local.common_tags
