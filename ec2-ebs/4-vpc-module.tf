34c10218f15aaecad4c5048871244380184131dd
Sudarsana K <sudak80@gmail.com>
2023-03-12 15:27:58 +0530

    commit5

--- ec2-ebs/4-vpc-module.tf
+++ ec2-ebs/4-vpc-module.tf
@@ -8,7 +8,7 @@ module "vpc" {
   name = "${local.name}-${var.vpc_name}"
   cidr = var.vpc_cidr_block
   azs             = var.vpc_availability_zones
-  public_subnets  = var.vpc_public_subnets
+  public_subnets  = element(var.vpc_public_subnets,0)
   #private_subnets = var.vpc_private_subnets
 
   # Database Subnets
