5931a9352925fbd48e296dc88ff66506c38787e6
Sudarsana K <sudak80@gmail.com>
2023-03-13 06:13:51 +0530

    commit6

--- ec2-ebs/3-variables.tf
+++ ec2-ebs/3-variables.tf
@@ -52,7 +52,7 @@ variable "vpc_availability_zones" {
 variable "vpc_public_subnets" {
   description = "VPC Public Subnets"
   type = list(string)
-  default = "10.0.101.0/24"
+  default = ["10.0.101.0/24"]
   #default = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
